7b69d4f090f43eab8d732345e8286e4fbc2d1f3c
Tobias (VRTX) <tobias@vrtxlabs.com>
2023-02-08 16:38:42 +0100

    Added C# implementation for swift UnweightedEdge struct

--- Core/UnweightedEdge.cs
+++ Core/UnweightedEdge.cs
@@ -4,4 +4,37 @@ using System.Text;
 
 namespace Core
 {
+
+    public struct UnweightedEdge : IEdge, IEquatable<UnweightedEdge>
+    {
+        public int u { get; set; }
+        public int v { get; set; }
+        public bool directed { get; set; }
+
+
+        #region Ctors
+        public UnweightedEdge(int u, int v, bool directed)
+        {
+            this.u = u;
+            this.v = v;
+            this.directed = directed;
+        }
+        #endregion
+
+
+        public bool Equals(UnweightedEdge other)
+        {
+            return this.u == other.u && this.v == other.v;
+        }
+
+        public IEdge reversed()
+        {
+            return new UnweightedEdge(v, u, directed);
+        }
+
+        public override string ToString()
+        {
+            return $"{u} -> {v}";
+        }
+    }
 }
