da8a5c4d2ae8322b70a8235a119bab3fa9cb6a47
Tobias (VRTX) <tobias@vrtxlabs.com>
2023-02-08 16:24:13 +0100

    Added C# interface for swift graph protocol

--- Core/Graph.cs
+++ Core/Graph.cs
@@ -4,4 +4,16 @@ using System.Text;
 
 namespace Core
 {
+
+    public interface IGraph<V, E> where V : IEquatable<V> where E: IEdge, IEquatable<E>
+    {
+        List<V> vertices { get; set; }
+        Dictionary<int, List<E>> edges { get; set; }
+
+        void init(ICollection<V> vertices);
+        bool addEdge(E edge, bool directed);
+
+    }
+
+
 }
