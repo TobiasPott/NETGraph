c6845d16b555c7839f3e98d727bb336d4fa3f423
Tobias Pott <mailme@tobiaspott.de>
2023-02-14 21:49:05 +0100

    Added run project to test library code
    Updated gitignore to ignore build outputs

---
+++ Runner/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Core;
+
+public class Node: IEquatable<Node>
+{
+    string name = string.Empty;
+    List<object> data = new List<object>();
+
+    public Node(string name)
+    {
+        this.name = name;
+    }
+
+    bool IEquatable<Node>.Equals(Node other)
+    {
+        if (other != null)
+            return other.name.Equals(this.name);
+        return false;
+    }
+
+    public override string ToString()
+    {
+        return $"{name} ({data.Count})";
+    }
+}
+
+public class MyGraph : GenericGraph<Node, UnweightedEdge>
+{
+    public MyGraph(ICollection<Node> vertices) : base(vertices)
+    { }
+}
+
+public class Program
+{
+
+    public const string VStart = "Start";
+    public const string VMid = "Mitte";
+    public const string VEnd = "Ende";
+
+    public static void Main(string[] args)
+    {
+
+        MyGraph g = new MyGraph(null);
+        int iStart = g.addVertex(new Node(VStart));
+        int iMid = g.addVertex(new Node(VMid));
+        int iEnd = g.addVertex(new Node(VEnd));
+
+        g.addEdge(new UnweightedEdge(iStart, iMid, true));
+        g.addEdge(new UnweightedEdge(iMid, iEnd, true));
+
+        Console.WriteLine(g.ToString());
+    }
+
+}
