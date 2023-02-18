670736e1e2502f17df4efb7cad761c740c63bcfa
Tobias Pott <mailme@tobiaspott.de>
2023-02-18 19:37:44 +0100

    Seaprated graph extension implementation into separate source files

---
+++ Core/Graph.Sort.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace NETGraph
+{
+    // ToDo: Continue working on left out utilities
+
+    // Stub class for missing helper and utility methods for different graph types and situations
+    //  *   [ ] Sort
+
+    public static class Sort
+    {
+    }
+}
+
---
+++ Core/Graph.Union.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NETGraph
+{
+
+    // ToDo: Continue working on left out utilities
+
+    // Stub class for missing helper and utility methods for different graph types and situations
+    //  *   [ ] Union
+
+    public static class Union
+    {
+
+    }
+}
