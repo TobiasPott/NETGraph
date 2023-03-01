71a2b0f199d7b17097c770ebceb631adadfe8251
Irmunsul <irmunsul@MBPvonIrmunsul.fritz.box>
2023-03-01 20:09:26 +0100

    Code cleanup

--- Core/Definition.Node.cs
+++ Core/Definition.Node.cs
@@ -8,15 +8,6 @@ namespace NETGraph.Core
     public abstract class NodeDefinition
     {
 
-        List<AccessorDefinition> accessors = new List<AccessorDefinition>();
-        List<DataDefinition> datas = new List<DataDefinition>();
-
-        protected NodeDefinition(IEnumerable<AccessorDefinition> accessors, IEnumerable<DataDefinition> datas)
-        {
-            this.accessors.AddRange(accessors);
-            this.datas.AddRange(datas);
-        }
-
 
     }
 }
