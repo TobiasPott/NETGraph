861bfc1dbfd722aab063b65f6740e027ce9b7d61
Irmunsul <irmunsul@MBPvonIrmunsul.fritz.box>
2023-03-19 14:05:33 +0100

    Renamed to Memory
    Added overload to generate data from typeIndex, type or generic typee

--- Core/Meta/Memory.cs
+++ Core/Meta/Memory.cs
@@ -18,13 +18,21 @@ namespace NETGraph.Core.Meta
                 return new ScalarData<T>(typeIndex, options);
         }
 
-        private static Type[] iDataDenericTypeCache = new Type[1];
-        private static Type[] iDataArgumentsTypeCache = new Type[] { typeof(int), typeof(IData.Options) };
-        private static object[] iDataArgumentsCache = new object[2];
-        // ToDo: add overload which takes int typeIndex and makes a lookup against the type registry
+        public static IData Generate(int typeIndex, IData.Options options)
+        {
+            Type type = MetaTypeRegistry.GetType(typeIndex);
+            return Generate(typeIndex, type, options);
+        }
         public static IData Generate(Type type, IData.Options options)
         {
             int typeIndex = MetaTypeRegistry.GetTypeIndex(type);
+            return Generate(typeIndex, type, options);
+        }
+        private static Type[] iDataDenericTypeCache = new Type[1];
+        private static Type[] iDataArgumentsTypeCache = new Type[] { typeof(int), typeof(IData.Options) };
+        private static object[] iDataArgumentsCache = new object[2];
+        private static IData Generate(int typeIndex, Type type, IData.Options options)
+        {
             // Specify the type parameter of the A<> type
             iDataDenericTypeCache[0] = type;
             Type genericType;
