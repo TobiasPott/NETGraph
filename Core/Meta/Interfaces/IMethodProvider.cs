ce6468375c0bda8c184491c165bc83df426fef92
Irmunsul <irmunsul@MBPvonIrmunsul.fritz.box>
2023-03-28 22:34:05 +0200

    Removed IMethodProvider interface (for now)

--- Core/Meta/Interfaces/IMethodProvider.cs
+++ Core/Meta/Interfaces/IMethodProvider.cs
@@ -6,13 +6,6 @@ namespace NETGraph.Core.Meta
 {
     public delegate IResolver MethodRef(IResolver reference, params IResolver[] inputs);
 
-    public interface IMethodProvider
-    {
-        // reference can be null for static calls, assignTo can be null, inputs can be null/empty
-        //void assign(string accessor, IResolver reference, IResolver assignTo, params IResolver[] inputs);
-        // reference can be null for static calls, result cannot be null, inputs can be null/empty
-        IResolver invoke(string accessor, IResolver reference, params IResolver[] args);
-    }
 
     // ToDo: Complete a list of methods of the string type to extract code
     //      include method name
