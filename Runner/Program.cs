0600a67d0a9f5d9bca43252aa6339302c59763d3
Irmunsul <irmunsul@MBPvonIrmunsul.fritz.box>
2023-04-19 22:29:24 +0200

    Updated runner test code

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -94,6 +94,15 @@ public class Program
         //MethodExtraction.ExtractMethod<string>("Concat", BindingFlags.Static | BindingFlags.Public, typeof(string), typeof(string));
         //MethodExtraction.ExtractMethod<string>("Replace", BindingFlags.Instance | BindingFlags.Public, typeof(string), typeof(string));
         //MethodExtraction.ExtractMethod<string>("ToLowerInvariant", BindingFlags.Instance | BindingFlags.Public);
+        //string extraction = MethodExtraction.ExtractMethod(typeof(Console), "WriteLine", BindingFlags.Static | BindingFlags.Public, typeof(string));
+        //Console.WriteLine(extraction);
+        /*
+         * Console library methods:
+         *  * WriteLine     
+         *  * Write         
+         *  * Clear         
+         */
+
 
         string code = "Int32 x = myInt.Add(\"Hello\", \"World!\");";
         code = "Int32 x = myInt.Add(\"sads\", y, myInt2.add(z, 3, anotherInt.add(4, 5)), \"(in, parenthesis)\", 'c', 10, anotherInt.add(6, 7));";
@@ -104,14 +113,16 @@ public class Program
                     + Environment.NewLine +
             "Int32 tmp2 = LibMath::Add(tmp, 12);";
 
+        code = "LibMath::WriteLine(\"Hello World!\")";
+
         List<Action> codeCall = JIT.CompileML(code);
 
         foreach (Action a in codeCall)
             a.Invoke();
         Console.WriteLine();
 
-        Console.WriteLine(Memory.Get("tmp"));
-        Console.WriteLine(Memory.Get("tmp2"));
+        //Console.WriteLine(Memory.Get("tmp"));
+        //Console.WriteLine(Memory.Get("tmp2"));
 
         Console.WriteLine();    
         Console.WriteLine();
